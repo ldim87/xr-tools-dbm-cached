447b022549a9670f5cacd3272b7b9cc96f114ab8
PandCar <pandcar@mail.ru>
2020-08-24 14:56:49 +0300

    DBMRollbackException
    Signed-off-by: PandCar <pandcar@mail.ru>

---
+++ src/DBMRollbackException.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @author Oleg Isaev (PandCar)
+ * @contacts vk.com/id50416641, t.me/pandcar, github.com/pandcar
+ */
+
+namespace XrTools;
+
+use \Exception;
+
+class DBMRollbackException extends Exception
+{
+
+}
+
